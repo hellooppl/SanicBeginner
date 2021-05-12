0319583dee7c4a3bd5d7e502eab690d9044063de
SAR-bang <palpaliadarsha@gmail.com>
2021-05-12 19:48:06 +0545

    routing managementy

--- entrypoint/app.py
+++ entrypoint/app.py
@@ -1,5 +1,6 @@
 from sanic import Sanic
 from sanic.response import text, json
+from . import service_layer,abstract
 
 app = Sanic(__name__)
 
@@ -10,6 +11,18 @@ async def handler(request):
         print(request.json["delivery_id"])
         return text('accepted')
     return text('OKAYY......')
+
+@app.route("/agent", methods=['GET', 'POST'])
+def add_new_agent(request):
+    service.add_shipping(validated_data=abstract.AddShipping(
+        category='food',
+        cost='202',
+        regionId=10,
+        orderId=22,
+        insurance=55.2,
+        date_to_ship= "2010-8-10"
+    ))
+    return Text("success")
 
 
 if __name__ == "__main__":
