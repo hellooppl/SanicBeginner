73d6023798618fab5880a464d26355289ecfbf15
SAR-bang <palpaliadarsha@gmail.com>
2021-05-18 00:53:02 +0545

    commands and messsage

--- domain/events.py
+++ domain/events.py
@@ -5,3 +5,17 @@ class Event():
 
 class NotAvailable(Event):
     user:UUID
+
+class ShippingCreated(Event):
+    _id: UUID
+    category: str
+    cost: float
+    regionId: int
+    orderId: int
+    insurance: float
+    date_to_ship: str    
+    is_deleted: bool=False
+
+class AllocationRequired(Event):
+    user:UUID
+    task:set()
